e352fa3b524d29fa1fd1dece85fe362e59685356
Kyle McKenna <mckennakyler@gmail.com>
2020-02-08 20:34:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 class Artist
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods 
-  extend Findable 
-  include Paramable 
+  extend Memorable::ClassMethods, Findable
+  include Memorable::InstanceMethods, Paramable 
+  # extend Findable 
+  # include Paramable 
   
   attr_accessor :name
   attr_reader :songs
