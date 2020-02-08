cdf90ea864c4d30d780795aa1d6c1f2afae2ce85
Kyle McKenna <mckennakyler@gmail.com>
2020-02-08 20:29:47 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
   
-  def reset_all
-    self.all.clear
-  end
-
-  def count
-    self.all.count
-  end
+    def count
+      self.all.count
+    end
+  
+  Module InstanceMethods 
+    def initialize 
+      
+    end   
+  end 
   
 end 
